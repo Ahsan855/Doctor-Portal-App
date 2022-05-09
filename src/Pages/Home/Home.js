911d6ca5e397d22ac3faa5bf6f240196def1b99a
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-10 00:04:08 +0600

    done Our section 3 card

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Bannar from './Bannar';
 import Info from './Info';
+import Services from './Services/Services';
 const Home = () => {
     return (
-        <div>
+        <div className='px-12'>
             <Bannar/>
             <Info/>
+            <Services/>
         </div>
     );
 };
--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -8,7 +8,7 @@ import phone from '../../assets/icons/phone.svg'
 const Info = () => {
     
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Opening Hours" img={clock} />
             <InfoCard bgClass="bg-accent" cardTitle="Our Locations" img={marker} />
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Contact Us" img={phone} />
